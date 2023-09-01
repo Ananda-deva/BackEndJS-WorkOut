aff77900c3b942d0d3ffd44c75a5c6b468c6c2f9
Ananda-deva <babichenko.a.v@mail.ru>
2023-09-01 15:48:19 +0700

    Добавил Log, упражнения(exercise) и тренеровки (workout), всё добавлено и обновлено в schema.prismaь

--- app/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import { authUser } from './auth/auth.controller.js'
-
-const router = express.Router()
-
-router.route('/login').post(authUser)
-
-export default router

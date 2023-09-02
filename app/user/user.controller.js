0c43d29d20152bf009925127ce3fb6de8481ee45
Ananda-deva <babichenko.a.v@mail.ru>
2023-09-02 12:07:46 +0700

    Реализовал показ тренировок у пользователя

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -14,5 +14,44 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 		select: UserFields
 	})
 
-	res.json(user)
+	const countExerciseTimeCompleted = await prisma.exerciseLog.count({
+		where: {
+			userId: req.user.id,
+			isCompleted: true
+		}
+	})
+
+	const kg = await prisma.exerciseTime.aggregate({
+		where: {
+			exerciseLog: {
+				userId: req.user.id
+			},
+			isCompleted: true
+		},
+		_sum: {
+			weight: true
+		}
+	})
+
+	const workouts = await prisma.workoutLog.count({
+		where: {
+			userId: user.id,
+			isCompleted: true
+		}
+	})
+
+	res.json([
+		{
+			label: 'Minutes',
+			value: Math.ceil(countExerciseTimeCompleted * 2.3)
+		},
+		{
+			label: 'Workouts',
+			value: workouts
+		},
+		{
+			label: 'KGS',
+			value: kg._sum.weight || 0
+		}
+	])
 })
